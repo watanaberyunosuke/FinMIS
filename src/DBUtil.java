1f452338ab86eefe78f151eb61b99d866f4032cd
Harry Watson <harryzhanchaoxi@gmail.com>
2019-02-10 15:06:20 +0800

    DBU

--- src/DBUtil.java
+++ src/DBUtil.java
@@ -1,3 +1,38 @@
+/* Input Module */
 
+package com.student.util;
+
+/* Import JDBC */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.student.AppConstants;
+
+/* Class Files */
 public class DBUtil {
+    private static DBUtil db;
+
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    private DBUtil(){
+
+    }
+}
+
+public static DBUtil getDbUtil() {
+    if(db == null){
+        db == new DBUtil();
+    }
+    return db;
+}
+
+public int executeUpdate(String sql) {
+    int result = -1;
+    if (getConn == )
 }
